9b550bcff5e6d69951b177ef04e90643b3c9132e
IMadeAgus <agsmade@gmail.com>
2025-04-09 22:44:17 +0800

    implement api city-controller and officespace-controller
    implement api city-controller and officespace-controller

--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
-    //
+    public function index()
+    {
+        $cities = OfficeSpace::with('city')->get();
+        return OfficeSpaceResource::collection($cities);
+    }
+
+    public function show(OfficeSpace $officeSpace)
+    {
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        $officeSpace->loadCount('officeSpaces');
+        return new OfficeSpaceResource($officeSpace);
+    }
 }
